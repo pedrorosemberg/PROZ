3973414071c7bc3854d08ba99f636f3c84be9fc1
Pedro Rosemberg <dev@pedrorosemberg.com>
2025-06-16 10:46:45 -0300

    upd

--- bd_houses/escola.sql
+++ bd_houses/escola.sql
@@ -1,1 +1,12 @@
 CREATE database escola;
+USE escola;
+CREATE TABLE alunos (
+	codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    endereco VARCHAR(100),
+    sala VARCHAR(50),
+    professor VARCHAR(100),
+    disciplina VARCHAR(100),
+    data DATE,
+    nota DECIMAL(4,2)
+);
